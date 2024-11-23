0c6b0affa62f14e814270f3dbed6e9011b05542f
raghu22556 <91258291+raghu22556@users.noreply.github.com>
2024-11-23 23:00:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import {resolve} from 'node:path'
+import path from 'path'
+import { resolve } from 'node:path'
 
 console.log(__dirname);
 
@@ -12,5 +12,10 @@ export default defineConfig({
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  },
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173, // Use the desired port
+    strictPort: true, // Ensure the specified port is used
   }
 })
